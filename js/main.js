32362d5f51e965edcafb0667d6286081e9da114c
Sylwia Fait <sylwia.fait@gmail.com>
2018-07-05 20:12:17 +0200

    Update CSS - added RWD

---
+++ js/main.js
@@ -0,0 +1,21 @@
+new Vue({
+    data(){
+        return {
+            title: "",
+            planet: "",
+            hostel: "",
+            language: "",
+            firstName: "",
+            secondName: "",
+            roomNumber: "",
+            neighborsHobby: "",
+            sightseeingPlace: "",
+            sightseeingMake: "",
+            dinnerPlace: "",
+            friends: "",
+            souvenirsPlace: "",
+            firstSouvenirs: "",
+            secondSouvenirs: ""
+        }
+    }
+}).$mount("#app");

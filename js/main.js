6b28e2d2952afc9ac00266460a2f7c134a585937
Sylwia Fait <sylwia.fait@gmail.com>
2018-07-12 14:27:37 +0200

    Update form and h3 position

--- js/main.js
+++ js/main.js
@@ -15,8 +15,7 @@ new Vue({
             friends: "",
             souvenirsPlace: "",
             firstSouvenirs: "",
-            secondSouvenirs: "",
-            link: "https://images.pexels.com/photos/1229942/pexels-photo-1229942.jpeg?auto=compress&cs=tinysrgb&h=650&w=940"
+            secondSouvenirs: ""
         }
     }
 }).$mount("#app");
